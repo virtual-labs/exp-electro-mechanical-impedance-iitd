c3b7d6e25c5e11e9f0e1d9ceccb5f5ec681c65fc
vleadadmin <admin@vlabs.ac.in>
2021-09-20 08:45:19 +0000

    https://virtual-labs.github.io/virtual-labs/exp-electro-mechanical-impedance-iitd click on the link to test your code.

---
+++ pretest.js
@@ -0,0 +1,194 @@
+/////////////////////////////////////////////////////////////////////////////
+
+/////////////////////// Do not modify the below code ////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////
+
+(function() {
+    function buildQuiz() {
+        // we'll need a place to store the HTML output
+        const output = [];
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            // we'll want to store the list of answer choices
+            const answers = [];
+
+            // and for each available answer...
+            for (letter in currentQuestion.answers) {
+                // ...add an HTML radio button
+                answers.push(
+                    `<label>
+          <input type="radio" name="question${questionNumber}" value="${letter}">
+          ${letter} :
+          ${currentQuestion.answers[letter]}
+        </label>`
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div>
+      <div class="answers"> ${answers.join("")} </div>`
+            );
+        });
+
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join("");
+    }
+
+    function showResults() {
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll(".answers");
+
+        // keep track of user's answers
+        let numCorrect = 0;
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            // find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            // if answer is correct
+            if (userAnswer === currentQuestion.correctAnswer) {
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                //answerContainers[questionNumber].style.color = "lightgreen";
+            } else {
+                // if answer is wrong or blank
+                // color the answers red
+                answerContainers[questionNumber].style.color = "red";
+            }
+        });
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+
+    const quizContainer = document.getElementById("quiz");
+    const resultsContainer = document.getElementById("results");
+    const submitButton = document.getElementById("submit");
+
+
+    /////////////////////////////////////////////////////////////////////////////
+
+    /////////////////////// Do not modify the above code ////////////////////////
+
+    /////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+    /////////////// Write the MCQ below in the exactly same described format ///////////////
+
+
+    const myQuestions = [{
+            question: "1. The dialogue box enables downloading the time and the ______ in the computer of the user.", ///// Write the question inside double quotes
+            answers: {
+                a: "Material density", ///// Write the option 1 inside double quotes
+                b: "Low frequency", ///// Write the option 2 inside double quotes
+                c: "Frequency domain data", ///// Write the option 1 inside double quotes
+                d: "Length", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "c" ///// Write the correct option inside double quotes
+        },
+
+        {
+            question: "2. The user may acquire the plots of the ______ and ______ against frequency using the LCR meter.", ///// Write the question inside double quotes
+            answers: {
+                a: "Conductance and susceptance", ///// Write the option 1 inside double quotes
+                b: "Time and frequency", ///// Write the option 2 inside double quotes
+                c: "Length and height", ///// Write the option 1 inside double quotes
+                d: "Time and frequency domain data", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
+
+        {
+            question: "3. Free-free” condition is ensured by hanging the beam _____ through a cello tape,", ///// Write the question inside double quotes
+            answers: {
+                a: "Vertically", ///// Write the option 1 inside double quotes
+                b: "Horizontally", ///// Write the option 2 inside double quotes
+                c: "Both a and b", ///// Write the option 1 inside double quotes
+                d: "None of the above", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
+
+        {
+            question: "4. From the frequency plot, the user can identify the _____ of the beam in axial mode.", ///// Write the question inside double quotes
+            answers: {
+                a: "4", ///// Write the option 1 inside double quotes
+                b: "natural frequency", ///// Write the option 2 inside double quotes
+                c: "5", ///// Write the option 1 inside double quotes
+                d: "6", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "b" ///// Write the correct option inside double quotes
+        },
+
+        {
+            question: "5. The user may plot G v/s frequency in ?", ///// Write the question inside double quotes
+            answers: {
+                a: "Pdf", ///// Write the option 1 inside double quotes
+                b: "MS Word", ///// Write the option 2 inside double quotes
+                c: "Excel", ///// Write the option 1 inside double quotes
+                d: "All of the above", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "c" ///// Write the correct option inside double quotes
+        },
+
+//         {
+//             question: "6. What is the apparatus required for the determination of split tensile strength of hardened concrete?", ///// Write the question inside double quotes
+//             answers: {
+//                 a: "CTM", ///// Write the option 1 inside double quotes
+//                 b: "Rebound hammer", ///// Write the option 2 inside double quotes
+//                 c: "VICAT", ///// Write the option 1 inside double quotes
+//                 d: "flexural testing machine", ///// Write the option 2 inside double quotes
+//             },
+//             correctAnswer: "a" ///// Write the correct option inside double quotes
+//         },
+        
+
+
+    // {
+    //   question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+    //   answers: {
+    //     a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
+    //     b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
+    //     c: "<img src='images/1c.PNG'>",      },
+    //   correctAnswer: "c"                ///// Write the correct option inside double quotes
+    // },
+
+
+        
+    ];
+
+
+
+
+    /////////////////////////////////////////////////////////////////////////////
+
+    /////////////////////// Do not modify the below code ////////////////////////
+
+    /////////////////////////////////////////////////////////////////////////////
+
+
+    // display quiz right away
+    buildQuiz();
+
+    // on submit, show results
+    submitButton.addEventListener("click", showResults);
+})();
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+/////////////////////// Do not modify the above code ////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ procedure.md
@@ -0,0 +1,95 @@
+### These steps will be followed for the experiment
+
+<!-- **PRE EXPERIMENT TASK**
+
+1) What is high frequency mode?
+2) What is LCR meter?
+3) What is conductance signature?
+4) What is conductance?
+5) What is susceptance?
+ -->
+
+Click on start experiment button to start the experiment.<br>
+<img src="images/s1.png"/><br>
+Step-1: (a) Click on add button to bring a stand.<br>
+<img src="images/s2.png"/><br>
+Step-1: (b) Click on add button to bring aluminium bar.<br>
+<img src="images/s3.png"/><br>
+Step-1: (c) Click on add button to bring paper cutter and tap.<br>
+<img src="images/s4.png"/><br>
+Step-1: (d) Click on Tap to stick it on aluminium bar.<br>
+<img src="images/s5.png"/><br>
+Step-1: (e) Click on paper cutter to cut the tap.<br>
+<img src="images/s6.png"/><br>
+Step-1: (f) Click on tap to place it back to its original position.<br>
+<img src="images/s7.png"/><br>
+Step-1: (g) Click on add button to bring a PZT Patch.<br>
+<img src="images/s8.png"/><br>
+Step-1: (h) Click on add button to bring bond.<br>
+<img src="images/s9.png"/><br>
+Step-1: (i) Click on brush to apply bond on aluminium bar.<br>
+<img src="images/s10.png"/><br>
+Step-1: (j) Click on PZT patch to place it on aluminium bar.<br>
+<img src="images/s11.png"/><br>
+Step-1: (k) Click on next button.<br>
+<img src="images/s12.png"/><br>
+Step-2: (a) Click on add button to bring soldering rod.<br>
+<img src="images/s13.png"/><br>
+Step-2: (b) Click on add button to bring wires.<br> 
+<img src="images/s14.png"/><br>
+Step-2: (c) Click on wire to attach it with PZT patch.<br> 
+<img src="images/s15.png"/><br>
+Step-2: (d) Click on another wire to attach it with PZT patch.<br> 
+<img src="images/s16.png"/><br>
+Step-2: (e) Click on PZT patch to see the zoom view of it.<br>
+<img src="images/s17.png"/><br>
+Step-2: (f) Click on soldering rod for soldering the wires.<br>
+<img src="images/s18.png"/><br>
+Step-2: (g) Click on next button.<br>
+<img src="images/s19.png"/><br>
+Step-3: (a) Click on add button to bring a PZT patch.<br>
+<img src="images/s20.png"/><br>
+Step-3: (b) Click on brush to apply bond on aluminium bar.<br>
+<img src="images/s21.png"/><br>
+Step-3: (c) Click on PZT patch to place it on aluminium bar.<br>
+<img src="images/s22.png"/><br>
+Step-3: (d) Click on add button to bring wires.<br>
+<img src="images/s23.png"/><br>
+Step-3: (e) Click on wire to attach it with PZT patch.<br>
+<img src="images/s24.png"/><br>
+Step-3: (f) Click on another wire to attach it with PZT patch.<br>
+<img src="images/s25.png"/><br>
+Step-3: (g) Click on PZT patch to see the zoom view of it.<br>
+<img src="images/s26.png"/><br>
+Step-3: (h) Click on soldering rod for soldering the wires.<br>
+<img src="images/s27.png"/><br>
+Step-3: (i) Click on next button.<br>
+<img src="images/s28.png"/><br>
+Step-4: (a) Click on add button to bring LCR.<br>
+<img src="images/s29.png"/><br>
+Step-4: (b) Click on aluminium bar to hanging it on the stand.<br>
+<img src="images/s30.png"/><br>
+Step-4: (c) Click black pins one by one to attach them with PZT patch wires.<br>
+<img src="images/s31.png"/><br>
+Step-4: (d) Now switch on the power button of LCR meter.<br>
+<img src="images/s32.png"/><br>
+Step-4: (e) Click on next button.<br>
+<img src="images/s33.png"/><br>
+Step-5: (a) Select the values and click on submit buttton.<br>
+<img src="images/s34.png"/><br>
+Step-5: (b) Click on ok button.<br>
+<img src="images/s35.png"/><br>
+Step-5: (c) Click on next button.<br>
+<img src="images/s36.png"/><br>
+Step-6: Calculate the natural frequency as per the theoritical formula given below:<br>
+
+Click on restart button.<br>
+<img src="images/s37.png"/><br>
+ 
+
+<!-- **POST EXPERIMENT TASK**
+
+1) Plot graph of conductance v/s frequency in MS Excel.
+2) Calculate resonant frequency of the beam from the above plot.
+3) From the plot, identify the natural frequency of the beam and compare the frequency obtained with theoretical frequency. 
+ -->
